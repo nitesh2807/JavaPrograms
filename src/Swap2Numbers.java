d7164e399efd9a49f484675d50e89774dda9c6b9
Nitesh Sigaram <sigaramnitesh482@gmail.com>
2020-08-21 09:10:51 -0700

    second commit

--- src/Swap2Numbers.java
+++ src/Swap2Numbers.java
@@ -8,17 +8,17 @@ public class Swap2Numbers {
 		int a=10;
 		int b=20;
 		System.out.println(" Before Swapping : " +a+" "+b);
-		/*int t=a;
+		int t=a;
 		a=b;
-		b=t;*/
+		b=t;
 		
 		/*a=a+b;
 		b=a-b;
 		a=a-b;*/
 		
-		a=a*b;
+		/*a=a*b;
 		b=a/b;
-		a=a/b;
+		a=a/b;*/
 		System.out.println(" After Swapping : " +a+" "+b);
 	}
 
